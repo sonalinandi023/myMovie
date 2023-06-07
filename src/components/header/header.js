9425d6dd2b6b64720061b9ca5478be2a26817625
sonalinandi023 <nandi.sonali023@gmail.com>
2023-06-07 20:18:05 +0530

    designing

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://media.istockphoto.com/id/1250059568/vector/fire-flame-icon-isolated-on-dark-background-fire-flame-logo-design-trendy-flat-style-vector.jpg?s=612x612&w=0&k=20&c=xn_V6NG8QGen3f_Nu47H0m2h4djtuRwvbblcNmc3xNM=" alt="logo" /></Link>
+                {/* <Link to="/"><img className="header__icon" src="https://media.istockphoto.com/id/1250059568/vector/fire-flame-icon-isolated-on-dark-background-fire-flame-logo-design-trendy-flat-style-vector.jpg?s=612x612&w=0&k=20&c=xn_V6NG8QGen3f_Nu47H0m2h4djtuRwvbblcNmc3xNM=" alt="logo" /></Link> */}
                 <Link to="/" style={{textDecoration: "none"}}><span>Home</span></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
